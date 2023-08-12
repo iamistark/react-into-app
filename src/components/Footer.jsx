459be881d2a8d6eb5cf774bcea49d85696f6ee66
Istiyaq Ahmad <istiyaqahmad7898@gmail.com>
2023-08-12 09:23:29 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+  <footer>
+    <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+    </div>
+    <div>
+        <h5>Follow me</h5>
+        <div>
+            <a href="https://www.linkedin.com/in/istiyaq-ahmad-265b51213" target={"blank"}>LinkdIn</a>
+            <a href="https://twitter.com/Istiyaq62378062?t=1vo-3nmd5uZ8XA-K277YIA&s=08" target={"blank"}>Twitter</a>
+            <a hrf="https://instagram.com/i__am_istark_21?igshid=ZDdkNTZiNTM=" target={"blank"}>Instagram</a>
+            <a href="https://www.facebook.com/istiyaq.ahmad.944" target={"blank"}>Facebook</a>
+            <a href="https://github.com/iamistark" target={"blank"}>Github</a>
+        </div>
+    </div>
+  </footer>
+  )
+}
+
+export default Footer
